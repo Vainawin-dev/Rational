6d167dd4ad96605816f1c843bfa100e08e9aca55
Vainawin Piyamitmongkol <112087678+Vainawin-dev@users.noreply.github.com>
2023-09-12 10:04:12 +0700

    Updated

---
+++ Rational-master/src/test/java/RationalTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RationalTest {
+    @Test
+    public void testAdd() {
+        Rational x = new Rational();
+        x.numerator = 1;
+        x.denominator = 2;
+        Rational y = new Rational();
+        y.numerator = 1;
+        y.denominator = 4;
+        x.add(y);
+        Assert.assertEquals(3, x.numerator);
+    }
+}
